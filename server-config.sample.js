4807c4552c56d9d5875689aaf9652694d1f591c2
John J. Camilleri <john@johnjcamilleri.com>
2016-06-11 08:05:14 +0200

    minors

--- server-config.sample.js
+++ server-config.sample.js
@@ -3,8 +3,8 @@ module.exports = {
   baseURL: '',
   fullBaseURL: 'http://localhost:3000',
   useCDN: false,
-  developmentMode: true,
-  maintenanceMode: false,
+  developmentMode: true, // turns off analytics etc
+  maintenanceMode: false, // put entire site down
   gabraURL: 'http://mlrs.research.um.edu.mt/resources/gabra',
   salt: '',
   dbUrl: 'localhost:27017/gabra',
