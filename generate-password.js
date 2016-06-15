08b06a13f8a2e6411390ee720af1b2c06b421bd7
John J. Camilleri <john@johnjcamilleri.com>
2016-06-15 06:56:58 +0200

    Add script for hashing passwords

---
+++ generate-password.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+// For generating hashed passwords to store in database
+// Usage: ./generate-password.js 1234
+
+var password = process.argv[2]
+if (!password) {
+  console.log('Empty password')
+  process.exit(1)
+}
+var config = require('./server-config')
+var salted = config.salt + password
+var shasum = require('crypto').createHash('sha1')
+var hashed = shasum.update(salted).digest('hex')
+console.log(hashed)
