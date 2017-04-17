3528c883078cb3afb4a5d52c1f04b7f5f324c662
bertugatt <bertugatt@gmail.com>
2017-04-17 16:31:34 +0200

    Utils directory for utility scripts

---
+++ utils/make-gloss-collection.js
@@ -0,0 +1,62 @@
+/*
+* Create a new collection in gabra DB:
+* Collection reverses lexemes, i.e. keys English gloss to lexeme ID
+*/
+
+//create the collection
+db.createCollection('glosses')
+
+
+//get all lexeme IDs
+var lids = db.lexemes.distinct('_id')
+
+for ( j = 0; j < lids.length; j++ ) {
+	var lex_id = lids[j]
+	var lex = db.lexemes.findOne({'_id': lex_id})
+	var gloss = lex.gloss
+
+	if (gloss != null) {
+
+		//split the glosses by newline
+		var glosses = gloss.split(/\r?\n/)
+		
+
+		for(i = 0; i < glosses.length; i++ ) {
+			g = glosses[i].toLowerCase();
+
+			//check if gloss is in the collection already
+			entry = db.glosses.findOne({'gloss': g})
+
+			if(entry == null ) {
+				db.glosses.insert({'gloss' : g, 'length': g.length, 'lexemes': [lex_id.str]})
+			
+			} else {
+				db.glosses.update({'gloss': g}, {$push: { 'lexemes': lex_id.str } })
+			}
+
+		}
+
+	}
+
+}
+
+//finally set up an index on the collection
+//db.glosses.createIndex( { gloss: "text" }, { score: {$meta: "textScore"} } )
+
+db.glosses.createIndex(
+   {
+     gloss: "text",
+   },
+   {
+     name: "GlossIndex"
+   }
+ )
+
+db.glosses.createIndex(
+   {
+     length: -1
+   },
+   {
+     name: "LengthIndex"
+ 	}
+ )
