7d491941baf73b264b1be9835f51b454d465dc62
John J. Camilleri <john@digitalgrammars.com>
2019-05-23 13:37:56 +0200

    Add `root.json` schema, test that all entries conform to it

--- test/schema.js
+++ test/schema.js
@@ -5,24 +5,34 @@ var config = require('../server-config.js')
 var monk = require('monk')
 var db = monk(config.dbUrl)
 
+var fs = require('fs')
+var Ajv = require('ajv')
+var ajv = new Ajv() // https://www.npmjs.com/package/ajv#options
+
 /* Tests data against schemas */
 describe('Schema', function () {
   // const server = require('../app')
 
-  it('receives data', function (done) {
+  describe('Roots', function () {
+    var schema = JSON.parse(fs.readFileSync('public/schemas/root.json'))
+    var validate = ajv.compile(schema)
 
-    db.get('roots').find({ type: 'strong' }, function (err, items) {
-      items.forEach(function (item) {
-        describe('root ID ' + item._id, function () {
-          it('doesn\'t contain j', function () {
-            item.radicals.should.not.containEql('j')
+    it('receives data', function (done) {
+      db.get('roots').find(function (err, items) {
+        items.forEach(function (item) {
+          describe(`${item.radicals} ${item.variant||'·'} (${item._id})`, function () {
+            it('conforms to schema', function () {
+              validate(item).should.be.true(formatErrors(validate.errors))
+            })
           })
         })
+        done()
       })
-      done()
-
     })
-
   })
-
 })
+
+function formatErrors (errors) {
+  if (!errors) return null
+  return errors.map(err => `${err.dataPath} ${err.message}`).join('\n')
+}
