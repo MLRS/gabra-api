c34963c0a6de61f49889fc688eb85e45ac444550
John J. Camilleri <john@johnjcamilleri.com>
2020-01-08 21:04:09 +0100

    Dump actual data in duplicates, include POS too

--- scripts/mongo/duplicates.js
+++ scripts/mongo/duplicates.js
@@ -1,6 +1,14 @@
-/* global db printjson */
-db.lexemes.aggregate(
-  { $group: { _id: '$lemma', total: {$sum: 1}, ids: {$push: '$_id'} } },
-  { $match: { total: { $gte: 2 } } },
-  { $sort: {total: -1} }
-).forEach(printjson)
+/* global db printjson print */
+var coll = db.getCollection('lexemes')
+coll.aggregate([
+  {$group: {
+    _id: {lemma: '$lemma', pos: '$pos'},
+    ids: {$addToSet: '$_id'},
+    count: {$sum: 1}
+  }},
+  {$match: {count: {'$gte': 2}}},
+  {$sort: {'_id.lemma': 1}}
+]).forEach((entry) => {
+  coll.find({_id: {$in: entry.ids}}).forEach(printjson)
+  print('----------')
+})
