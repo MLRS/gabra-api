0ab07767ebceac8fd16c9fb05a49df400c1d3b70
John J. Camilleri <john@digitalgrammars.com>
2020-01-21 00:05:32 +0100

    Access-Control-Allow-Headers: Content-Type

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public'), { maxAge: cache_static })
 // CORS
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
   next()
 })
 
