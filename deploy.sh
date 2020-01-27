8d5a267b95d1c677fcc2b2055d20ecd2df5ad696
John J. Camilleri <john@digitalgrammars.com>
2020-01-27 09:52:05 +0100

    Update deploy script to run npm install and restart service

--- deploy.sh
+++ deploy.sh
@@ -7,15 +7,21 @@ HOST=10.249.1.100
 REMOTEDIR=/var/www/public_html/resources/gabra-api/
 FLAGS="--recursive --checksum --compress --verbose --exclude-from=deploy-exclude.txt --log-file=deploy.log"
 
+set -e
+
 echo "Deploy Ġabra API"
 if [ "$1" = "-wet" ]; then
   echo "(For real)"
-  rsync           $FLAGS $LOCALDIR $HOST:$REMOTEDIR
+  rsync ${FLAGS} ${LOCALDIR} ${HOST}:${REMOTEDIR}
   echo
-  echo "### Remember you might need to run 'npm install' and 'pm2 restart' on the server ###"
+  echo "Running npm install..."
+  ssh ${HOST} "cd ${REMOTEDIR} && npm install"
+  echo
+  echo "Starting service..."
+  ssh pm2@${HOST} "cd ${REMOTEDIR} && ./start.sh"
 else
   echo "(Dry-run)"
-  rsync --dry-run $FLAGS $LOCALDIR $HOST:$REMOTEDIR
+  rsync --dry-run --delete ${FLAGS} ${LOCALDIR} ${HOST}:${REMOTEDIR}
   echo
   echo "### This was just a dry-run. To push for real, use the flag '-wet' ###"
 fi
