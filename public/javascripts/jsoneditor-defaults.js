36b65baa2661bb1d1c9c06955a499b19f6c1d23c
John J. Camilleri <john@johnjcamilleri.com>
2020-03-06 14:25:46 +0100

    Update JSON Editor, load on edit pages (WIP)

---
+++ public/javascripts/jsoneditor-defaults.js
@@ -0,0 +1,5 @@
+/* global JSONEditor */
+JSONEditor.defaults.options.theme = 'bootstrap4'
+JSONEditor.defaults.options.iconlib = 'fontawesome5'
+JSONEditor.defaults.options.remove_empty_properties = true
+JSONEditor.defaults.options.disable_edit_json = false
