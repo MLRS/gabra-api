f09b8a31d409ed6bc3cf367f575b9ad477a626ea
John J. Camilleri <john@digitalgrammars.com>
2020-04-12 15:29:01 +0200

    Move schema rendering into helper module. Prevent word wrap on field names.

--- routes/pages.js
+++ routes/pages.js
@@ -5,6 +5,7 @@ var monk = require('monk')
 const fs = require('fs')
 const marked = require('marked')
 const passport = require('passport')
+const loadSchemaAsHTML = require('./helpers/render-schema')
 
 /* GET home page */
 router.get('/home', function (req, res, next) {
@@ -238,58 +239,6 @@ function markdownPage (req, res, next, params) {
     }
     res.render('index', { title: params.title, body: content })
   })
-}
-
-/**
- * Read JSON schema from file and format as HTML table
- */
-function loadSchemaAsHTML (file, baseURL) {
-  let data = fs.readFileSync(`public/schemas/${file}`, 'utf8')
-  let stats = fs.statSync(`public/schemas/${file}`)
-  let html = `
-    <table class="table">
-    <thead>
-      <tr>
-        <th>Field</th>
-        <th>Type</th>
-        <th>Description</th>
-        <th style="width:40%">Example / allowed values</th>
-      </tr>
-    </thead>
-    <tbody>
-  `
-  data = JSON.parse(data)
-  for (let p in data.properties) {
-    let po = data.properties[p]
-    let eg = ''
-    if (po.examples) {
-      eg = po.examples.map(e => `<code>${JSON.stringify(e)}</code>`).join(',<br>')
-    } else if (po.enum) {
-      eg = po.enum.map(e => `<code>${JSON.stringify(e)}</code>`).join(' / ')
-    }
-    let refType
-    if (po['$ref'] && po['$ref'].startsWith('#/')) { // e.g. '#/definitions/agr'
-      let keys = po['$ref'].split('/').slice(1) // e.g. ['definitions','agr']
-      refType = keys.reduce((acc, val) => acc[val], data)['type']
-    }
-    html += `
-      <tr>
-        <td><code>${p}</code></td>
-        <td>${po.type || refType}</td>
-        <td>${po.description || ''}${data.required.includes(p) ? '<div class="small text-muted">Required</div>' : ''}</td>
-        <td>${eg}</td>
-      </tr>
-    `
-  }
-  html += `
-    </tbody>
-    </table>
-    <p>
-      Source: <a href="${baseURL}/schemas/${file}">${file}</a><br>
-      <small class="text-muted">Last updated ${stats.mtime.toISOString()}</small>
-    </p>
-  `
-  return html
 }
 
 module.exports = router
