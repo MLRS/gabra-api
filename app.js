22787c99cee84e793a774717fe7b012cca605dbc
Marc Tanti <marctanti@gmail.com>
2023-12-27 16:22:03 +0100

    update dependencies part 1
    Updated the following dependencies:
    "@forevolve/bootstrap-dark": "4.0.0"
    "@json-editor/json-editor": "2.13.1"
    "async": "3.2.5"
    
    Also removed body-parser because express does not require it any more.

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express')
 var path = require('path')
 var logger = require('morgan')
 var cookieParser = require('cookie-parser')
-var bodyParser = require('body-parser')
 var compression = require('compression')
 var helmet = require('helmet')
 var app = express()
@@ -16,8 +15,8 @@ app.locals.pretty = true
 
 app.use(helmet())
 app.use(logger('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 var cache_static = '7 days'
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: cache_static }))
